5d231cd1c29a67e2f6981d1ee444fb1936c2aba7
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2021-12-26 16:37:18 +0900

    별 찍기 - 3

---
+++ BaekJoon/Bronze 3/C++/2440.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int N;
+	std::cin >> N;
+
+	for (int i = N; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+			std::cout << "*";
+
+		std::cout << std::endl;
+	}
+}
