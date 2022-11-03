0c441848662360aabb4431cd520333e5321163a2
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-11-03 17:07:52 +0900

    피자 나눠 먹기 (1)

---
+++ Programmers/Level 0/C/피자 나눠 먹기 (1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    
+    while(n > 0) {
+        n -= 7;
+        answer++;
+    }
+    
+    return answer;    
+}
