4066bac53b36a3d2ab428d85596a5ee23bf2a933
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-11-20 11:58:33 +0900

    Create Binary_To_Decimal.cpp

---
+++ Math/진법 변환/Binary_To_Decimal.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int ToDecimal(int number);
+
+int main()
+{
+    int n;
+    
+    cout << "변환할 숫자 입력 : ";
+    cin >> n;
+    
+    cout << "10진수" << n << "에서 2진수 " << ToDecimal(n) << "변환" << endl;
+
+    return 0;
+}
+
+int ToDecimal(int number)
+{
+    int result = 0, n = 1;
+    
+    while(number > 0) {
+        
+        // 홀수일 경우 자릿수 값 확인하여 더하기
+        if(number % 2 != 0) {
+            
+            result += n;
+        }
+        
+        // 2진수 값을 더하기 위해 2씩 곱하기
+        n *= 2;
+        
+        // 1자리씩 확인하기 위해 나누기
+        number /= 10;
+    }
+    
+    return result;
+}
