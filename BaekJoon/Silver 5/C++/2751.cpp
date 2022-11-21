73f6d188527858c72db476a8e77c39b77e87f2f8
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-11-21 17:02:14 +0900

    수 정렬하기 2

---
+++ BaekJoon/Silver 5/C++/2751.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	  cin.tie(NULL);
+	  cout.tie(NULL);
+    
+    int N, number;
+    
+    vector<int> result;
+    
+    cin >> N;
+    
+    for(int i = 0; i < N; i++) {
+        cin >> number;
+        result.push_back(number);
+    }
+    
+    // 오름차순 정렬
+    sort(result.begin(), result.end());
+    
+    for(int i = 0; i < N; i++) {
+        cout << result[i] << '\n';
+    }
+
+    return 0;
+}
