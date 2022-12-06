aa6176500ec87a8ce8985b48da159a37a0d35a99
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-12-06 10:55:32 +0900

    카드2

---
+++ BaekJoon/Silver 4/C++/2164.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int N;
+    
+    queue<int> q;
+    
+    cin >> N;
+    
+    for(int i = 1; i <= N; i++) {
+        q.push(i);
+    }
+    
+    while(q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    
+    cout << q.front();
+
+    return 0;
+}
