d4fd8b1137a14ae36bb8208731d86bc26270d11d
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-12-06 15:38:49 +0900

    카드2

--- BaekJoon/Silver 4/C/2164.c
+++ BaekJoon/Silver 4/C/2164.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* que : 큐 데이터를 담을 배열
+   num : 현재 개수
+   front : 큐 배열에서 앞 인덱스 값
+   rear : 큐 배열에서 뒤 인덱스 값
+*/
+
 typedef struct _Queue
 {
     int *que;
@@ -29,9 +35,9 @@ int main()
     }
     
     while(Size(&q) > 1) {
-        Pop(&q);
-        Push(&q, Front(&q));
-        Pop(&q);
+        Pop(&q);  // 처음 카드 버림
+        Push(&q, Front(&q));  // 맨 앞의 카드 값을 뒤로 보냄
+        Pop(&q); // 다시 카드 버림
     }
     
     printf("%d\n", Front(&q));
@@ -50,24 +56,33 @@ void CreateQueue(Queue *q, int size)
 
 void Push(Queue* q, int data)
 {
+    // Push하여 총 개수 증가
     q->num++;
     
+    // 배열에 데이터 Push
     q->que[q->rear] = data;
+    
+    // 큐의 Rear 값 증가 배열이 1칸씩 뒤로 밀림
     q->rear++;
 }
 
 void Pop(Queue *q)
 {
+    // 총 개수 감소
     q->num--;
+    
+    // front값 증가 배열이 1칸씩 뒤로 밀림
     q->front++;
 }
 
 int Size(Queue *q)
 {
+    // 현재 개수
     return q->num;
 }
 
 int Front(Queue *q)
 {
+    // 현재 가장 앞의 값
     return q->que[q->front];
 }
