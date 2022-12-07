d11177ea005e38283e3fcece1e49a1c378faa0c5
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-12-07 15:55:39 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ BaekJoon/Bronze 5/C++/18108.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int y;
+    
+    cin >> y;
+    
+    cout << y - 543 << endl;
+    
+
+    return 0;
+}
