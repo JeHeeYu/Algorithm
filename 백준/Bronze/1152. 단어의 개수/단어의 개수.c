6b4731611692d2df7bad25574f1049b8d2d159a6
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-12-11 13:35:22 +0900

    [Bronze II] Title: 단어의 개수, Time: 12 ms, Memory: 1968 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <string.h>
+
+int main() {
+int cnt = 1;
+char S[1000000];
+scanf("%[^\n]s", S);
+for (int i = 0; i < strlen(S); i++) {
+if (S[i] == ' ')
+cnt++;
+}
+
+if (S[0] == ' ' && S[strlen(S) - 1] == ' ')
+cnt -= 2;
+else if (S[0] == ' ')
+cnt -= 1;
+else if (S[strlen(S) - 1] == ' ')
+cnt -= 1;
+
+printf("%d", cnt);
+}
