39c2f1f7cae0a10697100fb7df3c0b82ceaeaaa3
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-12-11 13:36:31 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5016 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int Min(int x, int y);
+int Result(int data);
+
+int arr[1000001];
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	printf("%d", Result(N));
+	
+	return 0;
+
+}
+
+int Min(int x, int y)
+{
+	return x > y ? y : x;
+}
+
+int Result(int data)
+{
+	int i;
+
+	for (i = 2; i <= data; i++)
+	{
+		arr[i] = arr[i - 1] + 1;
+		if (i % 2 == 0)
+			arr[i] = Min(arr[i], arr[i / 2] + 1);
+		if (i % 3 == 0)
+			arr[i] = Min(arr[i], arr[i / 3] + 1);
+	}
+
+	return arr[data];
+}
