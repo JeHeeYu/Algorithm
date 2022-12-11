0a830bac65a466a39c95b6bdf438615697d118e8
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-12-11 13:37:06 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	int N, M, sum = 0, min = 10001;
+	int arr[10001] = { 0 };
+	std::cin >> M;
+	std::cin >> N;
+
+	for (int i = 1; i <= N; i++)
+		arr[i] = i * i;
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (M <= arr[i] && arr[i] <= N)
+		{
+			sum += arr[i];
+			if (min > arr[i])
+				min = arr[i];
+		}
+	}
+
+	if (sum == 0)
+		std::cout << "-1" << std::endl;
+
+	else
+	{
+		std::cout << sum << std::endl;
+		std::cout << min << std::endl;
+	}
+
+	
+	return 0;
+}
