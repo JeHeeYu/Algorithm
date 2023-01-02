8f9ae17fc34bf31133b6f944455c4ef3c0967e65
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2023-01-02 10:10:00 +0900

    Add Comment

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// vector begin이 end보다 클 경우 true
 bool compare(int a, int b)
 {
     return a > b;
@@ -23,6 +24,7 @@ int main()
         v.push_back(number);
     }
     
+    // compare 함수를 이용해서 역순으로 정렬하도록 비교
     sort(v.begin(), v.end(), compare);
     
     for(int i = 0; i < N; i++) {
