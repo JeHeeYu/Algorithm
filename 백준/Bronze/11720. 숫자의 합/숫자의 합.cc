ee3588800db8372ce717c04514871c84fbdef024
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2023-04-01 18:00:57 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    string numbers;
+    
+    cin >> N;
+    cin >> numbers;
+    
+    int sum = 0;
+    
+    for(int i = 0; i < numbers.length(); i++) {
+        // string을 정수로 계산함
+        sum += numbers[i] - '0';
+    }
+    
+    cout << sum << "\n";
+
+    return 0;
+}
