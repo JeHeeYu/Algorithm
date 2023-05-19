8b26d2d7395294883a29d72682f8624ed7204ca5
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2023-05-19 15:07:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int i;
+    
+    for(i = 0; i < 500; i++)
+    {
+        if(num == 1)
+        {
+            return i;
+        }
+        
+        else if (num % 2 == 0)
+        {
+            num /= 2;
+        }
+        
+        else if(num % 2 == 1)
+        {
+            num = (num * 3) + 1;
+        }
+    }
+    return -1;
+}
