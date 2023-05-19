33c5906c61b775d6393b861ed51d8a0b5b3d0fac
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2023-05-19 15:07:21 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    
+    sort(d.begin(), d.end());
+    
+    for(int i = 0; i < d.size(); i++)
+    {
+        if(d[i] > budget)
+            break;
+       
+        budget -= d[i];
+       answer++;
+    }
+    return answer;
+}
